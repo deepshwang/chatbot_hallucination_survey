687a7f091eefdbc9fda9d954b6d56e3a0fcc19af
Sungwon Hwang <shwang.14@kaist.ac.kr>
2025-02-06 20:01:11 +0900

    d

--- app.py
+++ app.py
@@ -11,13 +11,18 @@ from contents.survey import *
 from contents.interaction import *
 from contents.post_interaction import *
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Initialize the page state in session_state
 if "page" not in st.session_state:
