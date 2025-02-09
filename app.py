de1931f1fa45ec98c4048d83d6babd3b2c0dbd3a
Sungwon Hwang <shwang.14@kaist.ac.kr>
2025-02-09 13:38:40 +0900

    d

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def main():
     # Set up responses state
     if "responses" not in st.session_state:
         st.session_state.responses = {}
-        query_params = st.query_params()
+        query_params = st.query_params
         if "PROLIFIC_PID" in query_params:
             st.session_state.responses["PROLIFIC_PID"] = query_params["PROLIFIC_PID"][0]
         else:
